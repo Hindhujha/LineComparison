6139559ebce95f6a7e7df05ad6f6136ca110603a
unknown <hindhujhasridhar@gmail.com>
2021-11-27 22:48:51 +0530

    Checking equality of 2 lines

--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -1,19 +1,31 @@
 ﻿namespace LineComp
 {
-    class UC0
+    class UC2
     {
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO LINE COMPARISON PROBLEM");
-            int X1, X2, Y1, Y2;
+            int X1, X2, X3, X4, Y1, Y2, Y3, Y4;
                 Console.WriteLine("ENTER THE COORDINATES:");
                 X1 = Convert.ToInt16(Console.ReadLine());
                 X2 = Convert.ToInt16(Console.ReadLine());
+                X3 = Convert.ToInt16(Console.ReadLine());
+                X4 = Convert.ToInt16(Console.ReadLine());
                 Y1 = Convert.ToInt16(Console.ReadLine());
                 Y2 = Convert.ToInt16(Console.ReadLine());
-                var result = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
-               Double length=Math.Round((Double)result,2);
-                Console.WriteLine("LENGTH OF THE LINE: " + length);
+                Y3 = Convert.ToInt16(Console.ReadLine());
+                Y4 = Convert.ToInt16(Console.ReadLine());
+            var result1 = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Double length1 = Math.Round((Double)result1, 2);
+            Console.WriteLine("LENGTH OF THE LINE: " + length1);
+
+            var result2 = Math.Sqrt(Math.Pow((X4 - X3), 2) + Math.Pow((Y4 - Y3), 2));
+            Double length2=Math.Round((Double)result2,2);
+                Console.WriteLine("LENGTH OF THE LINE: " + length2);
+
+            Console.WriteLine(length1.Equals(length2));
+            
+
 
             }
         }
